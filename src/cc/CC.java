71e3b8b2db6ea7738caf75c4137018bfd4a50ce7
luisgferrari <luisgferrari@hotmail.com>
2024-08-14 15:08:04 -0300

    Criação de nova TelaInicial

--- src/cc/CC.java
+++ src/cc/CC.java
@@ -1,5 +1,6 @@
 package cc;
 
+//import cc.view.TelaInicial;
 import cc.view.SelecaoDeArquivos;
 
 /**
@@ -12,8 +13,8 @@ public class CC {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        SelecaoDeArquivos janela = new SelecaoDeArquivos();
-        janela.setVisible(true);
+        new SelecaoDeArquivos().setVisible(true);
+//        new TelaInicial();
     }
 
 }
